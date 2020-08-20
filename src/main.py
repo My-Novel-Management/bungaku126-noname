4f7d0128fd5ca12b389edbb13d6e30977ccb1bce
nagisc007 <nagisc007@yahoo.co.jp>
2020-08-20 13:47:17 +0900

    [WIP] memo

--- src/main.py
+++ src/main.py
@@ -19,31 +19,31 @@ from config import ASSET
 #
 #   1. Initialize
 #   2. Story memo
-#   3. Structure    - 1/8
+#   3. Structure    - 1/8：3K
 #   4. Spec
-#   5. Plot         - 1/4
+#   5. Plot         - 1/4：7K
 #   6. Scenes
-#   7. Conte        - 1/2
+#   7. Conte        - 1/2：15K
 #   8. Layout
-#   9. Draft        - 1/1
+#   9. Draft        - 1/1：30K
 #
 ################################################################
 
 # Constant
-TITLE = "作品タイトル"
-MAJOR, MINOR, MICRO = 0, 0, 1
-COPY = "コピィ"
-ONELINE = "一行説明"
-OUTLINE = "あらすじ"
-THEME = "テーマ"
-GENRE = "ジャンル"
-TARGET = "ターゲット（年代）"
-SIZE = "規定サイズ"
-CONTEST_INFO = "コンテスト情報"
-CAUTION = "注意事項"
-NOTE = "備考"
+TITLE = "無名"
+MAJOR, MINOR, MICRO = 0, 1, 0
+COPY = "名前に振り回された人生でした"
+ONELINE = "約３万字の文学短編。有名になりたい男と無名になりたい女の人生が、交錯する"
+OUTLINE = "有名になりたい男は試行錯誤するけれど、その間にも同居するその界隈で有名な女は更に有名になり、無名になりたいと願う。二人は人生の交換を申し出た"
+THEME = "名前によって人生は左右されるのか？"
+GENRE = "文学"
+TARGET = "20-70years"
+SIZE = "原稿70から150枚（20Kから50K）"
+CONTEST_INFO = "第126回文學界新人賞"
+CAUTION = ""
+NOTE = ""
 SITES = ["エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム"]
-TAGS = ["ドラマ",]
+TAGS = ["文学",]
 RELEASED = (1, 1, 2020)
 
 
@@ -55,6 +55,61 @@ def ep_xxx(w: World):
 
 def ch_main(w: World):
     return w.chapter('main',
+            )
+
+
+# Notes
+def writer_note(w: World):
+    return w.writer_note("覚書",
+            )
+
+
+def plot_note(w: World):
+    return w.writer_note("プロットメモ",
+            "名前はまだない。という出だし",
+            "有名になることが人生の目標だったと書いている",
+            "残っていたブログを記者が読んでいる、というてい",
+            "無名でなくなった時に、主人公は後悔していた",
+            "「俺の名はない」とインタビュアーに答えて終わり",
+            )
+
+
+def chara_note(w: World):
+    return w.writer_note("人物メモ",
+            "有名になりたい男",
+            "有名になってる女",
+            "特に稼ぐとか有名になりたいとかの気持ちはなくゲームの実況配信を行っていたら、知らない間に有名になっていた",
+            "有名事件を追う記者",
+            "依頼されて事件の調査をし始めた。そこである動画配信者に辿り着く",
+            )
+
+
+def theme_note(w: World):
+    return w.writer_note("テーマメモ",
+            "有名と無名",
+            "有名というのは多くの人に名が知れているということ",
+            "言い換えれば有名であるとは知られてしまっている、ということ",
+            "知られていることのメリットは人気をベースとした商売に活用できる",
+            "デメリットは私生活に他人が侵食しやすい",
+            "また有象無象の色々な人間が近づいてくる",
+            "人生の防衛が難しくなる点か",
+            )
+
+
+def motif_note(w: World):
+    return w.writer_note("モチーフメモ",
+            "有名", "無名",
+            "炎上",
+            "SNS",
+            "メディア", "テレビ", "CM",
+            "本を出す",
+            "ポスター",
+            "名刺",
+            "顔出し",
+            "ストーカー",
+            "過保護な擁護者", "信者", "イエスマン", "宗教",
+            "自分の番組", "youtube",
+            "課金", "スパチャ", "ユーチューバー", "違法行為",
             )
 
 
@@ -79,6 +134,11 @@ def main(): # pragma: no cover
     w.config.set_taginfos(*TAGS)
     w.config.set_released(*RELEASED)
     return w.run(
+            writer_note(w),
+            plot_note(w),
+            chara_note(w),
+            theme_note(w),
+            motif_note(w),
             ch_main(w),
             )
 
