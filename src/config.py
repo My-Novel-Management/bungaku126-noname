97b39ad28e844192918ee540ff62f5d4bc2a9fec
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-08-25 10:26:30 +0900

    Add sub plot (#4)
    * Add sub plot
    
    * Add sub plot
    
    * [WIP] struct
    
    * [WIP] Fix plot
    
    * Add chara memo
    
    * Add chara hist
    
    * Add minor
    
    * Update v0.3.0[ci skip]

--- src/config.py
+++ src/config.py
@@ -8,8 +8,10 @@ ASSET = {
         "PERSONS": (
             # (tag / name / full / age (birth) / job / call / info)
             ('mume', '無名', '', 30,(1,1), 'male', '無職', "me:私"),
-            ("yume", "有奈", "", 30,(1,1), "female", "動画配信者", "me:わたし"),
+            ("yuna", "有奈", "冴喜,有奈", 30,(1,1), "female", "動画配信者", "me:わたし"),
+            ("ishi", "石脇", "石脇,和優", 35,(1,1), "male", "芸人", "me:俺"),
             ("hashi", "橋本", "", 40,(1,1), "male", "記者", "me:俺:k_me:私"),
+            ("mutsu", "睦美", "", 30,(1,1), "female", "イラストレータ", "me:うち"),
             ),
         "STAGES": (
             # (tag / name / parent / (geometry) / info)
@@ -25,6 +27,9 @@ ASSET = {
             ),
         "WORDS": (
             # (tag / name / cate / info)
+            ("theblog", "かつて有名だった私へ"),
+            ("line", "ＬＩＮＥ"),
+            ("pc", "ＰＣ"),
             ),
         "RUBIS": (
             # (origin / rubi / exclusions / always)
