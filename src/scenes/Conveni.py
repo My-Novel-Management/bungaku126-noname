5cd1e8c9a549c09182fc451a6ef50ae3dd9b1822
nagisc007 <nagisc007@yahoo.co.jp>
2020-09-11 11:10:13 +0900

    [WIP] Add conte

--- src/scenes/Conveni.py
+++ src/scenes/Conveni.py
@@ -12,6 +12,7 @@ from storybuilder.builder.world import World
 ## scenes
 def thought_of_famous(w: World):
     mume = w.get("mume")
+    rin = w.get("rin")
     return w.scene('有名への思い',
             w.cmd.change_stage("Conveni"),
             w.cmd.change_time("evening"),
@@ -19,6 +20,11 @@ def thought_of_famous(w: World):
             mume.do("店内の本棚には漫画誌が並ぶ"),
             mume.do("有名になってください、と言われたことがひっかかっている"),
             mume.do("弁当と飲み物を買ってレジに並ぶ"),
-            # TODO
+            rin.be("アジア系の顔立ちの店員がレジにいて、さばさばした素振りで客対応している"),
+            mume.do("店員の名札に『$rin』と書かれている"),
+            mume.think("それを見てなんと読むのだろうか考えている",
+                "りん、なのか、はやし、なのか",
+                "ずっと利用しているけれど未だに読み方が分からない",
+                "それはひょっとしたら無名と大差ないのではないだろうか、と考えたり"),
             )
 
